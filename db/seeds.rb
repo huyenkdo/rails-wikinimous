4f984fbfcb8238a072832392ec1d6020f19d430a
Huyen Do <huyenkdo@gmail.com>
2024-08-06 17:38:00 +0200

    coded index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Book.title, content: Faker::Hipster.paragraph_by_chars(characters: 256, supplemental: false))
+end
